b54ed4dcf24b35ab7e14f0353b2fc97b4d0aa281
Yulia Cech <yulia.cech@elastic.co>
2021-12-28 21:46:40 +0100

    Update with the position at Elastic, remove mentoring from the experience section

--- src/portfolio-components/MetaHead.jsx
+++ src/portfolio-components/MetaHead.jsx
@@ -4,7 +4,7 @@ import {Helmet} from "react-helmet";
 
 class MetaHead extends React.Component {
   render() {
-    const items = [];
+    const items = [<meta charSet="utf-8" />, <meta lang="en"/>];
     if (this.props.config.meta && this.props.config.meta.title) {
       items.push(<title key={items.length + 1}> {this.props.config.meta.title}</title>);
     }
@@ -12,17 +12,9 @@ class MetaHead extends React.Component {
       items.push(<meta key={items.length + 1} name="description" content={this.props.config.meta.description}/>);
     }
     if (this.props.config.font && this.props.config.font.link) {
-
       items.push(<link key={items.length + 1} href={this.props.config.font.link} rel="stylesheet"/>);
     }
-    if (items.length > 0) {
-      return (
-        <Helmet>
-          {items}
-        </Helmet>
-      );
-    }
-    return null;
+    return (<Helmet>{items}</Helmet>);
   }
 }
 
